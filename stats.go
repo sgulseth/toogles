03ba945edd7b143130808ad828b0b911c970d5c7
Sindre Gulseth <sindre.gulseth@vg.no>
2016-09-17 00:04:45 +0200

    Include goroutines in stats for debugging

--- stats.go
+++ stats.go
@@ -2,18 +2,21 @@ package main
 
 import (
     "time"
+    "runtime"
 )
 
 type Stats struct {
     Connections    int32
     Features       int
     Uptime         int32
+    Routines       int
 }
 
 var stats = Stats {
     Connections: 0,
     Features: 0,
     Uptime: 0,
+    Routines: 0,
 }
 
 var started = int32(time.Now().Unix())
@@ -21,6 +24,7 @@ var started = int32(time.Now().Unix())
 func GetStats() Stats {
     now := int32(time.Now().Unix())
     stats.Uptime = now - started
+    stats.Routines = runtime.NumGoroutine()
 
     return stats
 }
